bdc64b815cf7df35b5bb99e9e0d19a329a7f8eaa
blue_jam <blue-jam@users.noreply.github.com>
2025-02-10 03:14:14 +0900

    Change last day for prediction

--- cron.sh
+++ cron.sh
@@ -9,12 +9,13 @@ cd "$(dirname "$0")"
 mkdir -p images
 
 last_day_of_last_year=$(date -d "$(date +%Y-01-01) -1 day" +%Y-%m-%d)
+last_day_of_this_year=$(date -d "$(date +%Y-01-01) +1 year -1 day" +%Y-%m-%d)
 last_day_of_last_month=$(date -d "$(date +%Y-%m-01) -1 day" +%Y-%m-%d)
 last_day_of_this_month=$(date -d "$(date +%Y-%m-01) +1 month -1 day" +%Y-%m-%d)
 this_year=$(date +%Y)
 this_month=$(date +%Y-%m)
 
-./iknow-tools plot -predict-completed "$last_day_of_last_year" "$last_day_of_this_month"
+./iknow-tools plot -predict-completed "$last_day_of_last_year" "$last_day_of_this_year"
 mv plot.png "images/${this_year}.png"
 
 ./iknow-tools plot -predict-completed "$last_day_of_last_month" "$last_day_of_this_month"
